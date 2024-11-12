cb34d491ab6d2407764b968e1de88f94414fe9cf
Alexandra Savino <avs2167@barnard.edu>
2024-11-12 12:06:09 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import DottedBackground from './generals/DottedGraph'; 
 // import NavBar from './generals/NavBar';
